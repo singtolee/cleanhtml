8253fc4ce26fe291fc38217aac6e0c427c0861bb
singtolee <liqiang0618@outlook.com>
2018-07-16 17:01:27 +0700

    robost regular expression

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ const ofile = args.outputFilePath;
 jsonfile.readFile(inFile,function(err,obj){
     var i;
     for(i=0;i<obj.length;i++){
-        obj[i].detail = strtoarray(obj[i].detail)
+        obj[i].detail = strtoarray(obj[i].detail);
+		obj[i].status = false;
     }
 
     jsonfile.writeFile(ofile,obj,{spaces: 2, EOL: '\r\n'}, function(err){
@@ -26,6 +27,21 @@ jsonfile.readFile(inFile,function(err,obj){
 })
 
 function strtoarray(str){
-    var res = str.match(/https:\/\/cbu01.alicdn.com\/img\/ibank\/\d+\/\d+\/\d+\/\d+\_\d+.jpg/g)
-    return res;
+    //var res = str.match(/https:\/\/cbu01.alicdn.com\/img\/ibank\/\d+\/\d+\/\d+\/\d+\_\d+.jpg/g)
+	//https://img.alicdn.com/imgextra/i3/876014952/TB2ZizbXQblJKJjSszeXXaAYXXa_!!876014952.jpg
+	//cbu01.alicdn.com/img/ibank/2016/456/840/3739048654_1434148681.jpg"
+	
+	var res1 = str.match(/\/\/cbu01.alicdn.com\/img\/ibank\/\d+\/\d+\/\d+\/\d+\_\d+.jpg/g)
+	var res2 = str.match(/\/\/img.alicdn.com\/imgextra\/i\d\/\d+\/\w+\_!!\d+.jpg/g)
+	
+	if(res1 && res2 ){
+		return res1.concat(res2)
+	}else {
+		if(res1){
+			return res1
+		}else {
+			return res2
+		}
+		
+	}
 }
