847c457e7ab857b0eaefcacc875df9067e0ab46c
singtolee <liqiang0618@outlook.com>
2018-07-17 13:04:32 +0700

    new regex

--- app.js
+++ app.js
@@ -30,9 +30,16 @@ function strtoarray(str){
     //var res = str.match(/https:\/\/cbu01.alicdn.com\/img\/ibank\/\d+\/\d+\/\d+\/\d+\_\d+.jpg/g)
 	//https://img.alicdn.com/imgextra/i3/876014952/TB2ZizbXQblJKJjSszeXXaAYXXa_!!876014952.jpg
 	//cbu01.alicdn.com/img/ibank/2016/456/840/3739048654_1434148681.jpg"
-	
+	//img.alicdn.com/imgextra/i2/415761/TB28502dQfb_uJkSne1XXbE4XXa-415761.jpg
+	//img.alicdn.com/imgextra/i3/876014952/TB2ZizbXQblJKJjSszeXXaAYXXa_!!876014952.jpg
+	//img.alicdn.com/imgextra/i4/318494464/TB2yZZEaiGO.eBjSZFPXXcKCXXa_!!318494464.jpg"
+	//img.alicdn.com/imgextra/i2/415761/TB28502dQfb_uJkSne1XXbE4XXa-415761.jpg
+	//img.alicdn.com/imgextra/i2/415761/TB28502dQfb_uJkSne1XXbE4XXa-415761.jpg" 
+	//img.alicdn.com/imgextra/i1/415761/TB2ZkB3dQfb_uJkHFrdXXX2IVXa-415761.jpg"
+	/*
 	var res1 = str.match(/\/\/cbu01.alicdn.com\/img\/ibank\/\d+\/\d+\/\d+\/\d+\_\d+.jpg/g)
 	var res2 = str.match(/\/\/img.alicdn.com\/imgextra\/i\d\/\d+\/\w+\_!!\d+.jpg/g)
+	var res3 = str.match(/\/\/img.alicdn.com\/imgextra\/i\d\/\d+\/[\w_-]\d+.jpg/g)
 	
 	if(res1 && res2 ){
 		return res1.concat(res2)
@@ -44,4 +51,6 @@ function strtoarray(str){
 		}
 		
 	}
+	*/
+	return str.match(/\/\/\S*.jpg/g)
 }
